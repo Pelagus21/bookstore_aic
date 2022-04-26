26f8263959afc5c2f808db7c502f066236f47685
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2022-04-26 15:19:45 +0300

    Registration logic done

--- app/controller.js
+++ app/controller.js
@@ -14,6 +14,7 @@ client.connect();
 
 let failed = false;
 let exists = false;
+let userDTO = {};
 
 exports.getLoginForm = function(req, res) {
     res.render(path.resolve(__dirname + '/../templates/login.twig'), { failed : failed });
@@ -47,10 +48,43 @@ exports.logIn = function (req, res) {
 }
 
 exports.getRegistrationForm = function(req, res) {
-    res.render(path.resolve(__dirname + '/../templates/registration.twig'), {userExists : exists});
+    res.render(path.resolve(__dirname + '/../templates/registration.twig'), {userExists : exists, userDTO : userDTO});
     exists = false;
+    userDTO = {};
 }
 
 exports.registerUser = function(req, res) {
-    //let queryStr = 'INSERT INTO public."Customers" ("Login", "First_name", "Last_name")'
+    let queryStr = 'INSERT INTO public."Customers" ("Login", "Password", "Birth_date", "Email"';
+    if(req.body.first_name != "")
+        queryStr += ', "First_name"';
+    if(req.body.surname != "")
+        queryStr += ', "Surname"';
+    if(req.body.last_name != "")
+        queryStr += ', "Last_name"';
+    queryStr += ') values(\''+ req.body.username + '\', \'' + req.body.password + '\', \'' +
+        req.body.birth_date + '\', \'' + req.body.email + '\'';
+    if(req.body.first_name != "")
+        queryStr += ', \'' + req.body.first_name + '\'';
+    if(req.body.surname != "")
+        queryStr += ', \'' +req.body.surname + '\'';
+    if(req.body.last_name != "")
+        queryStr += ', \'' + req.body.last_name + '\'';
+    queryStr += ');';
+    console.log(queryStr);
+    client.query(queryStr, (err, result) => {
+        if(!err){
+            console.log("User registered!");
+            return res.redirect('/home');
+        }
+        console.log(err);
+        exists = true;
+        userDTO.first_name = req.body.first_name;
+        userDTO.surname = req.body.surname;
+        userDTO.last_name = req.body.last_name;
+        userDTO.email = req.body.email;
+        userDTO.birth_date = req.body.birth_date;
+        userDTO.password = req.body.password;
+        return res.redirect('/registration')
+    });
+
 }
