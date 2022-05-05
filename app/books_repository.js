bc830a60c6231670f80f8d6b027020087910e9e5
xOooQOFU <oleksandra.shliakhova@ukma.edu.ua>
2022-05-05 03:09:57 +0300

    Added orders(without db insertion for now)

--- app/books_repository.js
+++ app/books_repository.js
@@ -40,3 +40,15 @@ exports.deleteBook = function (id) {
     let queryStr = 'DELETE FROM "Books" WHERE "Id" = \'' + id + '\';';
     return db.query(queryStr);
 }
+
+exports.getBookWithFullInfo = async function (id) {
+    let book = (await exports.getBookById(id)).rows;
+    if(book.length === 0)
+        return [];
+    let authors = (await exports.getAuthorsOfBook(id)).rows;
+    let genres = (await exports.getGenresOfBook(id)).rows;
+    book[0].authors = authors;
+    book[0].genres = genres;
+
+    return book[0];
+}
